1b2bb007c72cf20dfaf031f5b5555602b58eb1d5
reason-knowledge <franckngoubounkou@gmail.com>
2026-04-19 10:03:31 +0100

    Mise ajour de background du footer

--- src/components/custom/footer.tsx
+++ src/components/custom/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
   }
 
   return (
-    <footer className="bg-gray-50">
+    <footer className="footer-background">
       <div className="max-w-6xl mx-auto px-6 py-16 md:py-20 relative">
         <button
           onClick={scrollToTop}
